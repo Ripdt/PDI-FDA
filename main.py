aad73d6a612754abd2322d2a53881069519f6169
Filipi da Costa <filipiscalvindacosta@gmail.com>
2024-09-13 20:13:40 -0300

    gauss with sobel examples

--- main.py
+++ main.py
@@ -18,15 +18,19 @@ cv2.imshow('askpy-noise', img_noise)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-cv2.imshow('gauss', gauss_filter(img=img_noise))
+img_gauss = gauss_filter(img=img_noise);
+img_gauss = gauss_filter(img=img_gauss);
+img_gauss = gauss_filter(img=img_gauss);
+
+cv2.imshow('gauss', img_gauss)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 kernel_sobel_1 = np.array(([-1,-2,-1],[0,0,0],[1,2,1]))
 kernel_sobel_2 = np.array(([-1,0,1],[-2,0,2],[-1,0,1]))
 
-img_sobel_1 = conv2d_sharpening(img_noise, kernel_sobel_1)
-img_sobel_2 = conv2d_sharpening(img_noise, kernel_sobel_2)
+img_sobel_1 = conv2d_sharpening(img_gauss, kernel_sobel_1)
+img_sobel_2 = conv2d_sharpening(img_gauss, kernel_sobel_2)
 
 img_sobel = np.abs(img_sobel_1)+np.abs(img_sobel_2)
 
