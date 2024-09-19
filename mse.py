a389d35771da769fa6af96c1f7c55a736b27b2cd
immidddddddd <107276790+immidddddddd@users.noreply.github.com>
2024-09-19 14:33:17 -0300

    mean squared error

---
+++ mse.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def calculate_mse(original: np.ndarray, compressed: np.ndarray) -> float
+    # Diferença entre as imagens
+    erro = original.astype(np.float64) - compressed.astype(np.float64)
+    # Cálculo MSE
+    mse = np.mean(err ** 2)
+    return mse
